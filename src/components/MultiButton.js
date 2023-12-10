071e3365763ed06628e474026376c234cad05936
OchiengJr <chrispine.ochieng@student.moringaschool.com>
2023-12-10 16:41:46 +0300

    Event Handling in React

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -7,9 +7,17 @@ function MultiButton() {
 
   return (
     <div>
-      <button onClick={()=>handleClick(1)}>Button 1</button>
-      <button onClick={()=>handleClick(2)}>Button 2</button>
-      <button onClick={()=>handleClick(3)}>Button 3</button>
+      {/* Using handleClick function directly */}
+      <button onClick={() => handleClick(1)}>Button 1</button>
+      <button onClick={() => handleClick(2)}>Button 2</button>
+      <button onClick={() => handleClick(3)}>Button 3</button>
+
+      {/* Reusing handleClick function with a loop */}
+      {[1, 2, 3].map((number) => (
+        <button key={number} onClick={() => handleClick(number)}>
+          Button {number}
+        </button>
+      ))}
     </div>
   );
 }
