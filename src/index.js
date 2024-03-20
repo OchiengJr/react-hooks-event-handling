d21b0011d64f4dd96ca686487c3829a65d72f8e6
OchiengJr <chrispine.ochieng@student.moringaschool.com>
2024-03-20 09:20:14 +0300

    Event Handling in React

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Render the App component into the DOM
+ReactDOM.render(<App />, document.getElementById('root'));
